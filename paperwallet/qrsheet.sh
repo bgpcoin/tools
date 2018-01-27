b1aa0ea714012f457326dea3ee8687c978de0220
BGPCoin Developer <dev@bgpcoin.com>
2018-01-27 08:33:06 +0000

    beautiful

--- paperwallet/qrsheet.sh
+++ paperwallet/qrsheet.sh
@@ -14,17 +14,17 @@ for addr in $(bgpcoind listunspent | grep address | cut -f4 -d\" | sort | uniq)
 do
 cat >> $TMP/walletsheet.html <<EOF
 <table>
-<tr><td>$c</td><th>Address</th><th>Amount</th><th>Private Key</th></tr>
+<tr><th>Address</th><th>Amount</th><th>Private Key</th></tr>
 EOF
-	qrencode -o $TMP/a${c}.png $addr
+	qrencode -m 4 -o $TMP/a${c}.png $addr
 	key=$(grep $addr $TMP/keys | awk '{print $1}')
 	numbers=$(bgpcoind listunspent 6 9999999 [\"$addr\"] | grep amount | cut -f2 -d: | sed -s 's/,/+/' )
 	amount=$(echo ${numbers}-0 | bc)
-	qrencode -o $TMP/k${c}.png $key
+	qrencode -m 4 -o $TMP/k${c}.png $key
 
 cat >> $TMP/walletsheet.html <<EOF
-<tr><td>$c</td><td><img src="a${c}.png"></td><td><img src="bgp480.png"></td><td><img src="k${c}.png"></td></tr>
-<tr><td>$c</td><td class="address">$addr</td><td>$amount</td><td class="key">$key</td></tr>
+<tr><td><img src="a${c}.png"></td><td><img src="bgp480.png"></td><td><img src="k${c}.png"></td></tr>
+<tr><td class="address">$addr</td><td>$amount</td><td class="key">$key</td></tr>
 </table>
 <hr />
 EOF
